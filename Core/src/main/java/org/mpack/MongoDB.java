3af676d607062252bca1a6e4bc8bb2bc9ab60208
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-04-19 18:08:22 +0200

    Normlization of URLS and some bugs in main

--- Core/src/main/java/org/mpack/MongoDB.java
+++ Core/src/main/java/org/mpack/MongoDB.java
@@ -2,9 +2,12 @@ package org.mpack;
 
 import com.mongodb.client.*;
 import com.mongodb.client.model.UpdateOptions;
+
 import java.util.Collections;
+
 import org.bson.Document;
 import org.bson.types.ObjectId;
+
 import java.util.List;
 import java.util.Set;
 
@@ -12,15 +15,16 @@ public class MongoDB {
     MongoCollection<org.bson.Document> urlsCollection;
     MongoCollection<org.bson.Document> stateCollection;
 
-    static final  String CONNECTION_STRING = "mongodb://localhost:27017";
+    static final String CONNECTION_STRING = "mongodb://localhost:27017";
     MongoDatabase searchEngineDb;
     MongoClient mongoClient;
+
     MongoDB() {
         initConnection();
 
     }
 
-    public  void initConnection() {
+    public void initConnection() {
         try {
             //SearchEngine
             //.
@@ -30,7 +34,7 @@ public class MongoDB {
 
             urlsCollection = searchEngineDb.getCollection("CrawledURLS");
             stateCollection = searchEngineDb.getCollection("State");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
@@ -43,10 +47,10 @@ public class MongoDB {
     }
 
     // state is 0 if not finished or 1 if finished
-    public void setState(int state){
+    public void setState(int state) {
         // should be called on a new
         org.bson.Document urlEntry = new org.bson.Document("Name", "state");
-        org.bson.Document updateEntry = new org.bson.Document("isDone",state);
+        org.bson.Document updateEntry = new org.bson.Document("isDone", state);
         org.bson.Document setEntry = new org.bson.Document("$set", updateEntry);
 
         UpdateOptions options = new UpdateOptions().upsert(true);
@@ -54,64 +58,75 @@ public class MongoDB {
         stateCollection.updateOne(urlEntry, setEntry, options);
 
     }
-    public int getState(){
+
+    public int getState() {
         org.bson.Document searchEntry = new org.bson.Document("Name", "state");
         org.bson.Document op = Document.parse("{isDone: 1 ,_id: 0}");
 
-        Document doc =  stateCollection.find(searchEntry).projection(op).first();
-        if (doc == null){
+        Document doc = stateCollection.find(searchEntry).projection(op).first();
+        if (doc == null) {
             //no state document is found
-            return  -1;
+            return -1;
         }
         return (int) doc.get("isDone");
     }
-    public long getUrlCount(){
+
+    public void deleteState() {
+        org.bson.Document searchEntry = new org.bson.Document("Name", "unprocessed");
+        stateCollection.deleteOne(searchEntry);
+    }
+
+    public long getUrlCount() {
 
         return urlsCollection.countDocuments();
 
     }
-    public void addToStateArray(String url){
+
+    public void addToStateArray(String url) {
         org.bson.Document urlEntry = new org.bson.Document("Name", "unprocessed");
         // push the url to the links
-        org.bson.Document updateEntry = new org.bson.Document("$push", new org.bson.Document("links",url));
+        org.bson.Document updateEntry = new org.bson.Document("$push", new org.bson.Document("links", url));
         UpdateOptions options = new UpdateOptions().upsert(true);
 
-        stateCollection.updateOne(urlEntry,updateEntry,options);
+        stateCollection.updateOne(urlEntry, updateEntry, options);
     }
-    public void removeFromStateArray(String url){
+
+    public void removeFromStateArray(String url) {
         org.bson.Document urlEntry = new org.bson.Document("Name", "unprocessed");
         // push the url to the links
-        org.bson.Document updateEntry = new org.bson.Document("$pull", new org.bson.Document("links",url));
+        org.bson.Document updateEntry = new org.bson.Document("$pull", new org.bson.Document("links", url));
 
-        stateCollection.updateOne(urlEntry,updateEntry);
+        stateCollection.updateOne(urlEntry, updateEntry);
     }
 
-    public List<String> getStateArray(){
+    public List<String> getStateArray() {
         org.bson.Document searchEntry = new org.bson.Document("Name", "unprocessed");
         org.bson.Document op = Document.parse("{links: 1 ,_id: 0}"); //only get the array of links
 
-        Document doc =  stateCollection.find(searchEntry).projection(op).first();
-        if (doc == null){
+        Document doc = stateCollection.find(searchEntry).projection(op).first();
+        if (doc == null) {
             //no state document is found
             return Collections.emptyList();
         }
         return doc.getList("links", String.class);
 
     }
-    public void addToSuggestionsArray(String query){
+
+    public void addToSuggestionsArray(String query) {
         org.bson.Document urlEntry = new org.bson.Document("Name", "suggestions");
         // TODO: ensure addToSet  is working
-        org.bson.Document updateEntry = new org.bson.Document("$addToSet", new org.bson.Document("old_searches",query));
+        org.bson.Document updateEntry = new org.bson.Document("$addToSet", new org.bson.Document("old_searches", query));
         UpdateOptions options = new UpdateOptions().upsert(true);
 
-        stateCollection.updateOne(urlEntry,updateEntry,options);
+        stateCollection.updateOne(urlEntry, updateEntry, options);
     }
-    public List<String> getSuggestionsArray(){
+
+    public List<String> getSuggestionsArray() {
         org.bson.Document searchEntry = new org.bson.Document("Name", "suggestions");
         org.bson.Document op = Document.parse("{old_searches: 1 ,_id: 0}"); //only get the array of links
 
-        Document doc =  stateCollection.find(searchEntry).projection(op).first();
-        if (doc == null){
+        Document doc = stateCollection.find(searchEntry).projection(op).first();
+        if (doc == null) {
             //no state document is found
             return Collections.emptyList();
         }
@@ -119,7 +134,7 @@ public class MongoDB {
 
     }
 
-    public void getVisitedLinks(Set<String> arr){
+    public void getVisitedLinks(Set<String> arr) {
 
         arr.clear();
         for (String s : urlsCollection.distinct("url_link", String.class)) {
