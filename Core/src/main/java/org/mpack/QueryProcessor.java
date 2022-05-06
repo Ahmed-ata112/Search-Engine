628a6b935581c1f34c665c4cdbd78f6a76fddf36
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-05-06 17:49:24 +0200

    doaa

--- Core/src/main/java/org/mpack/QueryProcessor.java
+++ Core/src/main/java/org/mpack/QueryProcessor.java
@@ -1,4 +1,5 @@
 package org.mpack;
+
 import ca.rmen.porterstemmer.PorterStemmer;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -11,41 +12,39 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class QueryProcessor {
-    HashMap<Integer,ArrayList<Document>> result = new HashMap<>();
+    static HashMap<Integer, ArrayList<Document>> result = new HashMap<>();
     String Phrase;
-    HashMap<Character, List<String>> stopWords = new HashMap<>();
+    static HashMap<Character, List<String>> stopWords = new HashMap<>();
     MongoClient mongoClient;
     MongoDatabase DataBase;
-    MongoCollection<org.bson.Document> InvertedDocs;
-    MongoCollection<org.bson.Document> StemmingCollection;
-    public QueryProcessor()
-    {
+    static MongoCollection<org.bson.Document> InvertedDocs;
+    static MongoCollection<org.bson.Document> StemmingCollection;
+
+    public QueryProcessor() {
         InitMongoDb();
     }
 
-    private void InitMongoDb()
-    {
+    private void InitMongoDb() {
         mongoClient = MongoClients.create(MongodbIndexer.CONNECTION_STRING);
         DataBase = mongoClient.getDatabase("SearchEngine");
         InvertedDocs = DataBase.getCollection("InvertedFile");
         StemmingCollection = DataBase.getCollection("StemmingCollection");
     }
 
-    private @NotNull String Stem(String Word)
-    {
+    private @NotNull String Stem(String Word) {
         String StemmedWord;
         PorterStemmer stem = new PorterStemmer();
         StemmedWord = stem.stemWord(Word);
         return StemmedWord.toLowerCase();
     }
 
-    public HashMap<Integer,ArrayList<Document>> QueryProcessingFunction (String SearchQuery) throws FileNotFoundException {
+    public HashMap<Integer, ArrayList<Document>> QueryProcessingFunction(String SearchQuery) throws FileNotFoundException {
         stopWords = Indexer.constructStopWords();
         List<String> SearchTokens = List.of(SearchQuery.split(" "));
-        Indexer.removeStopWords(SearchTokens,stopWords);
+        Indexer.removeStopWords(SearchTokens, stopWords);
         ArrayList<Document> OriginalWords = new ArrayList<>();
         ArrayList<Document> StemmedWords = new ArrayList<>();
-        for (int i=0;i<SearchTokens.size();i++) {
+        for (int i = 0; i < SearchTokens.size(); i++) {
             //1- retrieve the original word document and put it in hashmap
             Document Doc = InvertedDocs.find(new Document("token_name", SearchTokens.get(i))).first();
             if (Doc != null) {
@@ -64,24 +63,27 @@ public class QueryProcessor {
                     StemmedWords.add(Doc);
                 }
             }
-            result.putIfAbsent(0,OriginalWords);
-            result.putIfAbsent(1,StemmedWords);
+            result.putIfAbsent(0, OriginalWords);
+            result.putIfAbsent(1, StemmedWords);
+
+
         }
+
+
         return result;
     }
 
-    public HashMap<Integer,Document> PhraseProcessing (String SearchQuery) {
-        //1-construct and remove stop words
-        //2-split the phrase into array of words
-        //3-search for a certain word in the database
-        //4-parse retrieved URLs
-        //5- if it contain the whole phrase put it into the hashmap else parse the next URL
-        return result;
-    }
+//    public HashMap<Integer,Document> PhraseProcessing (String SearchQuery) {
+//        //1-construct and remove stop words
+//        //2-split the phrase into array of words
+//        //3-search for a certain word in the database
+//        //4-parse retrieved URLs
+//        //5- if it contain the whole phrase put it into the hashmap else parse the next URL
+//        return result;
+//    }
 
 
-    public static void main(String[] arg)
-    {
+    public static void main(String[] arg) {
         QueryProcessor q = new QueryProcessor();
     }
 }
