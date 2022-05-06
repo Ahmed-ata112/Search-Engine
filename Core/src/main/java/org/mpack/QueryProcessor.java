4ffae4ab3220c63e32173eb6b27764b17aef0679
fady2001 <75928317+fady2001@users.noreply.github.com>
2022-05-06 16:32:46 +0200

    Update QueryProcessor.java

--- Core/src/main/java/org/mpack/QueryProcessor.java
+++ Core/src/main/java/org/mpack/QueryProcessor.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class QueryProcessor {
@@ -37,31 +39,45 @@ public class QueryProcessor {
         return StemmedWord.toLowerCase();
     }
 
-    public HashMap<Integer,Document> QueryProcessingFunction (String SearchQuery) {
-        //1- retrieve the original word document and put it in hashmap
-        Document Doc = InvertedDocs.find(new Document("token_name", SearchQuery)).first();
-        if (Doc != null) {
-            result.putIfAbsent(0, Doc);
-        }
-        //2-stemming process
-        String StemmedWord = Stem(SearchQuery);
-        //3- retrieve documents for stemmed words from stemming collection
-        Doc = StemmingCollection.find(new Document("stem_word", StemmedWord)).projection(Document.parse("{Equivalent_words: 1 ,_id: 0}")).first();
-        if (Doc != null) {
-            //4-make an array list of stemming words
-            ArrayList<String> arr = (ArrayList<String>) Doc.get("Equivalent_words");
-            for (String s : arr) {
-                //5- retrieve documents for stemmed words from inverted file and put it in the hashmap
-                Doc = InvertedDocs.find(new Document("token_name", s)).first();
-                result.putIfAbsent(1, Doc);
+    public HashMap<Integer,Document> QueryProcessingFunction (String SearchQuery) throws FileNotFoundException {
+        stopWords = Indexer.constructStopWords();
+        List<String> SearchTokens = List.of(SearchQuery.split(" "));
+        Indexer.removeStopWords(SearchTokens,stopWords);
+        for (int i=0;i<SearchTokens.size();i++) {
+            //1- retrieve the original word document and put it in hashmap
+            Document Doc = InvertedDocs.find(new Document("token_name", SearchTokens.get(i))).first();
+            if (Doc != null) {
+                result.putIfAbsent(0, Doc);
+            }
+            //2-stemming process
+            String StemmedWord = Stem(SearchTokens.get(i));
+            //3- retrieve documents for stemmed words from stemming collection
+            Doc = StemmingCollection.find(new Document("stem_word", StemmedWord)).projection(Document.parse("{Equivalent_words: 1 ,_id: 0}")).first();
+            if (Doc != null) {
+                //4-make an array list of stemming words
+                ArrayList<String> arr = (ArrayList<String>) Doc.get("Equivalent_words");
+                for (String s : arr) {
+                    //5- retrieve documents for stemmed words from inverted file and put it in the hashmap
+                    Doc = InvertedDocs.find(new Document("token_name", s)).first();
+                    result.putIfAbsent(1, Doc);
+                }
             }
         }
         return result;
     }
 
+    public HashMap<Integer,Document> PhraseProcessing (String SearchQuery) {
+        //1-construct and remove stop words
+        //2-split the phrase into array of words
+        //3-search for a certain word in the database
+        //4-parse retrieved URLs
+        //5- if it contain the whole phrase put it into the hashmap else parse the next URL
+        return result;
+    }
+
+
     public static void main(String[] arg)
     {
         QueryProcessor q = new QueryProcessor();
-        q.QueryProcessingFunction("cancel");
     }
 }
