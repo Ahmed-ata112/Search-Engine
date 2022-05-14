167a6d2ee2bdd9cbbe2b928a83bc7b392c1b8d57
rufaida-kassem <rufaidahkassem@gmail.com>
2022-05-14 05:52:50 +0200

    .

--- Core/src/main/java/org/mpack/Try.java
+++
@@ -1,9 +0,0 @@
-package org.mpack;
-
-public class Try {
-    public static void main(String[] args)
-    {
-        String parag = "If you enjoyed hi sa asd ads sdef fsjd this frightening Replay then be sure to check out our recent episodes fsd fsdrie erwor fsdfs featuring Dead Space 2 or Amnesia: d";
-        System.out.println(Ranker.text(parag, "Amnesia: d", parag.indexOf("Amnesia: d")));
-    }
-}
